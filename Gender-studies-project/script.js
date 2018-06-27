6b6ee4d2ef6f9508650d71bc8bbcf68eeedbd468
JazzyLazzy <37636333+JazzyLazzy@users.noreply.github.com>
2018-06-26 21:16:36 -0700

    Added new animation

--- Gender-studies-project/script.js
+++ Gender-studies-project/script.js
@@ -1,5 +1,6 @@
 var slide0 = document.getElementById("welcome");
-var slide1 = document.getElementById("crash");
+var slide0$5 = document.getElementById("airplane");
+var slide1 = document.getElementById("airplanecrash");
 var slide2 = document.getElementById("soiboi");
 var slide3 = document.getElementById("commman");
 var slide4 = document.getElementById("gather");
@@ -14,22 +15,22 @@ var slide11 = document.getElementById("likepeter");
 var slide12 = document.getElementById("redpill");
 var slide13 = document.getElementById("obama");
 
-/*Oh! No JQuery? It is because JQuery is not necessary.*/
+/*Oh! No JQuery? It is because JQuery is not necessary. JQuery is... JUST PLAIN BOGUS.*/
 
-var slidearr = [slide0, slide1, slide2, slide3, slide4, slide5, slide6, slide6$5, slide7, slide8, slide9, slide10, slide11, slide12, slide13];
+var slidearr = [slide0, slide0$5, slide1, slide2, slide3, slide4, slide5, slide6, slide6$5, slide7, slide8, slide9, slide10, slide11, slide12, slide13];
 var i = 0;
 
 var planecrash = new Audio("aud/747crash.mp3");
 var ussranthem = new Audio("aud/soviet-anthem.mp3");
 
 function pauseplanecrash() {	
-	if (slide2.style.display == "block") {
+	if (slide1.style.display == "none") {
 		planecrash.pause();
 	}
 }
 
 function pauseussranthem() {
-	if (slide4.style.display == "block") {
+	if (slide3.style.display == "none") {
 		ussranthem.pause();
 	}
 }
@@ -38,9 +39,29 @@ function slideplays(){
 	if (slide0.style.display == "block") {
 		document.getElementById("backward").style.display = "none";
 	}
-	if (slide1.style.display == "block") {
+	if (slide0$5.style.display == "block") {
 		document.getElementById("backward").style.display = "block";
-		planecrash.play();
+	}
+	if (slide1.style.display == "block") {
+		var planemove = document.getElementById("crashplane");
+		var planepos = 50;
+		var id9 = setInterval(frame5, 3);
+		function frame5(){
+			if (planepos == 461) {
+				clearInterval(id9);
+				document.getElementById("crashplane").style.display = "none";
+				slide1.style.backgroundImage = "url('img/911.jpg')";
+			}
+			if (planepos == 388) {
+				planecrash.play();
+				planepos++;
+				planemove.style.left = planepos + "px";
+			}
+			else {
+				planepos++;
+				planemove.style.left = planepos + "px";
+			}
+		}
 	}
 	if (slide3.style.display == "block") {
 		ussranthem.play();
@@ -177,6 +198,8 @@ function slideplays(){
 	}
 	if (slide12.style.display == "block") {
 		document.getElementById("forward").style.display = "block";
+		var benshapiro = new Audio("aud/benshapiro.mp3");
+		benshapiro.play();
 	}
 	if (slide13.style.display == "block") {
 		var obaama = new Audio("aud/obaama.mp3");
@@ -185,10 +208,74 @@ function slideplays(){
 	}
 }
 
+var txt0$5 = "There was an airplane.";
+var txt0$75 = "No one knows why, but it crashed."
+var txt1 = "No one knows why, but it crashed.";
+var txt2 = "soy boy justin (no caps because that is a symbol of oppression) is the first survivor to get up. He started fumbling around for his communist manifesto.";
+var txt3 = "When he found it, he couldn't help but sing in joy. Союз нерушимый...";
+var txt4 = "As the remaining survivors woke up, they gathered around him, as he had the communist manifesto. They decided to elect him supreme leader.";
+var txt5 = "soy boy justin immediately begin implementing a system in order for the survivors to... keep surviving. He decided to split the jobs up equally, making 50% of the hunters female, the rest being male, and 50% of the gatherers, and caretakers of the 2 children, male, the rest being female.";
+var txt6 = "However, after a week, productivity wasn't as high as exepected. The female hunters, IN TOTAL, only brought in 78% of what their male counterparts brought in, and the male gatherers and caretakers were 22% less efficient at their assigned roles than their female counterparts.";
+var txt6$5 = "The combat veteran told children this story.";
+var txt7 = "At this point. The stranded islanders were starving. This is what their situation looked like:";
+var txt8 = "One man, named Peter Jordanson, decided to declare the situation just plain bogus.";
+var txt9 = "Jordanson decided to consult with the financial advisor, the most intelligent of the survivors, as to where to go on the island in order to best survive. She was assigned a hunter, and faired pretty well at it.";
+var txt10 = "Then with a small group, Jordanson split off and said:";
+var txt11 = "Jordanson's group did better than justin's group, and therefore survived. justin died a horrible death.";
+
+var slide0$5txt = document.getElementById("airplanetxt");
+var slide1txt = document.getElementById("crashtxt");
+var slide2txt = document.getElementById("soiboitxt");
+var slide3txt = document.getElementById("commmantxt");
+var slide4txt = document.getElementById("gathertxt");
+var slide5txt = document.getElementById("soiboisplittxt");
+var slide6txt = document.getElementById("noresourcetxt");
+var slide6$5txt = document.getElementById("bad-jobstxt");
+var slide7txt = document.getElementById("starvetxt");
+var slide8txt = document.getElementById("peterjordansontxt");
+var slide9txt = document.getElementById("financialadvisetxt");
+var slide10txt = document.getElementById("mericatxt");
+var slide11txt = document.getElementById("likepetertxt");
+
+var x = 0;
+var txtcontentarr = [txt0$5, txt1, txt2, txt3, txt4, txt5, txt6, txt6$5, txt7, txt8, txt9, txt10, txt11];
+var y = 0;
+var txtarr = [slide0$5txt, slide1txt, slide2txt, slide3txt, slide4txt, slide5txt, slide6txt, slide6$5txt, slide7txt, slide8txt, slide9txt, slide10txt, slide11txt];
+var txtspeed = 55;
+
+function showtxt() {
+	window.addEventListener("keypress", speed, false);
+	window.addEventListener("keyup", slow, false);
+	function speed(e) {	
+		if (e.keyCode == 32) {
+			txtspeed--;
+		}
+	}
+	function slow(ev) {
+		if (ev.keyCode == 32) {
+			txtspeed = 55;
+		}
+	}
+	if (y === txtarr.length){
+		y = txtarr.length;
+	}
+	else if (x < txtcontentarr[y].length) {
+		txtarr[y].innerHTML += txtcontentarr[y].charAt(x);
+		x++;
+		setTimeout(showtxt, txtspeed);
+	}
+	else if (x = txtcontentarr[y].length) {
+		y = y + 1;
+		x = 0;
+		txtspeed = 55;
+	}
+}
+
 document.getElementById("forward").onclick = function() {
 	i = i + 1;
 	slidearr[i - 1].style.display = "none";
 	slidearr[i].style.display = "block";
+	showtxt();
 	slideplays();
 	pauseplanecrash();
 	pauseussranthem();
@@ -201,6 +288,8 @@ document.getElementById("backward").onclick = function() {
 	slideplays();
 	pauseplanecrash();
 	pauseussranthem();
+	document.getElementById("crashplane").style.display = "block";
+	slide1.style.backgroundImage = "url('img/towers.jpg')";
 }
 
 function ajs() {
@@ -208,9 +297,11 @@ function ajs() {
 	var aj2 = new Audio("aud/Alex 3.mp3");
 	var aj3 = new Audio("aud/Alex 6.mp3");
 	var aj4 = new Audio("aud/Alex 5.mp3");
+	var aj5 = new Audio("aud/alexjones7");
 	
-	var ajarr = [aj1, aj2, aj3, aj4];
+	var ajarr = [aj1, aj2, aj3, aj4, aj5];
 	var randajs = ajarr[Math.floor(Math.random() * ajarr.length)];
+	console.log(randajs);
 	randajs.play();
 }
 
